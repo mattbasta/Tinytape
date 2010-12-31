df24117a6d386a8fcc6f60210fd0e080f232899f
Matt Basta <me@mattbasta.com>
2010-12-31 02:02:42 -0500

    Napping graphic for when Redis is offline

--- libraries/redis.php
+++ libraries/redis.php
@@ -1,6 +1,31 @@
 <?php
 
-$r = new Redis();
-$r->connect('192.168.140.217', 6379);
-$r->auth(file_get_contents('/var/auth/redis'));
-view_manager::set_value("redis", $r);
+try {
+	$r = new Redis();
+	$r->connect('192.168.140.217', 6379);
+	$r->auth(file_get_contents('/var/auth/redis'));
+	view_manager::set_value("redis", $r);
+} catch (Exception $e) {
+	?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Tinytape is sleeping...</title>
+<style type="text/css">
+#sleeping {
+	font-family:sans-serif;
+	text-align:center;
+	padding:300px;
+	background:url("/images/sleeping.png") no-repeat center 30px;
+}
+</style>
+</head>
+<body>
+<div id="sleeping">
+	<p>Hey bud, Tinytape is down for a nap. Yeah, a nap.<br />Come back in a few.</p>
+</div>
+</body>
+</html>
+	<?php
+	exit;
+}
