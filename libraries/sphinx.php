dd4a80aafa71aa1af6a99fa2016567b8795317d5
Matt Basta <me@mattbasta.com>
2011-03-14 02:35:03 -0400

    Made Sphinx optional if not installed

--- libraries/sphinx.php
+++ libraries/sphinx.php
@@ -1,7 +1,9 @@
 <?php
 
-$sphinx = new SphinxClient();
-$sphinx->setServer("localhost", 9312);
-$sphinx->setMatchMode(SPH_MATCH_ANY);
-$sphinx->setMaxQueryTime(10);
-view_manager::set_value("sphinx", $sphinx);
+if(SEARCH_PROVIDER == "sphinx") {
+	$sphinx = new SphinxClient();
+	$sphinx->setServer("localhost", 9312);
+	$sphinx->setMatchMode(SPH_MATCH_ANY);
+	$sphinx->setMaxQueryTime(10);
+	view_manager::set_value("sphinx", $sphinx);
+}
